e7e7510281c7d53e504bdcafdf84b805db697f25
TheRealJackiBoi <jslam@oulund.dk>
2023-05-08 10:49:02 +0200

    added new startcode for database

---
+++ src/main/resources/fogstartcode.sql
@@ -0,0 +1,280 @@
+CREATE DATABASE  IF NOT EXISTS `fog_carport_test` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `fog_carport_test`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fog_carport_test
+-- ------------------------------------------------------
+-- Server version	8.0.32-24
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `item_list`
+--
+
+DROP TABLE IF EXISTS `item_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item_list` (
+  `item_list_id` int NOT NULL AUTO_INCREMENT,
+  `use_description` varchar(45) DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `order_id` int NOT NULL,
+  PRIMARY KEY (`item_list_id`),
+  UNIQUE KEY `item_list_id_UNIQUE` (`item_list_id`),
+  KEY `fk_item_list_orders1_idx` (`order_id`),
+  CONSTRAINT `fk_item_list_orders1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_list`
+--
+
+LOCK TABLES `item_list` WRITE;
+/*!40000 ALTER TABLE `item_list` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `material_variants`
+--
+
+DROP TABLE IF EXISTS `material_variants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `material_variants` (
+  `variant_id` int NOT NULL AUTO_INCREMENT,
+  `length` double NOT NULL,
+  `quantity` int NOT NULL,
+  `materials_material_id` int NOT NULL,
+  `item_list_item_list_id` int NOT NULL,
+  PRIMARY KEY (`variant_id`),
+  UNIQUE KEY `variant_id_UNIQUE` (`variant_id`),
+  KEY `fk_material_variants_materials1_idx` (`materials_material_id`),
+  KEY `fk_material_variants_item_list1_idx` (`item_list_item_list_id`),
+  CONSTRAINT `fk_material_variants_item_list1` FOREIGN KEY (`item_list_item_list_id`) REFERENCES `item_list` (`item_list_id`),
+  CONSTRAINT `fk_material_variants_materials1` FOREIGN KEY (`materials_material_id`) REFERENCES `materials` (`material_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material_variants`
+--
+
+LOCK TABLES `material_variants` WRITE;
+/*!40000 ALTER TABLE `material_variants` DISABLE KEYS */;
+/*!40000 ALTER TABLE `material_variants` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materials`
+--
+
+DROP TABLE IF EXISTS `materials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materials` (
+  `material_id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) NOT NULL,
+  `materialscol` varchar(45) DEFAULT NULL,
+  `unit` varchar(45) NOT NULL,
+  `unit_price` double DEFAULT NULL,
+  `type` varchar(45) NOT NULL,
+  PRIMARY KEY (`material_id`),
+  UNIQUE KEY `material_id_UNIQUE` (`material_id`),
+  KEY `fk_materials_units1_idx` (`unit`),
+  KEY `fk_materials_type1_idx` (`type`),
+  CONSTRAINT `fk_materials_type1` FOREIGN KEY (`type`) REFERENCES `type` (`type`),
+  CONSTRAINT `fk_materials_units1` FOREIGN KEY (`unit`) REFERENCES `units` (`unit`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materials`
+--
+
+LOCK TABLES `materials` WRITE;
+/*!40000 ALTER TABLE `materials` DISABLE KEYS */;
+/*!40000 ALTER TABLE `materials` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `material_cost` double NOT NULL,
+  `sales_price` double NOT NULL,
+  `c_width` double NOT NULL,
+  `c_length` double NOT NULL,
+  `c_height` double NOT NULL,
+  `user_id` int NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `s_width` double DEFAULT NULL,
+  `s_length` double DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `fk_orders_user1_idx` (`user_id`),
+  KEY `fk_orders_status1_idx` (`status`),
+  CONSTRAINT `fk_orders_status1` FOREIGN KEY (`status`) REFERENCES `status` (`status`),
+  CONSTRAINT `fk_orders_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`role`),
+  UNIQUE KEY `role_UNIQUE` (`role`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `status` (
+  `status` varchar(45) NOT NULL,
+  PRIMARY KEY (`status`),
+  UNIQUE KEY `status_UNIQUE` (`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `type`
+--
+
+DROP TABLE IF EXISTS `type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `type` (
+  `type` varchar(45) NOT NULL,
+  PRIMARY KEY (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `type`
+--
+
+LOCK TABLES `type` WRITE;
+/*!40000 ALTER TABLE `type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `units`
+--
+
+DROP TABLE IF EXISTS `units`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `units` (
+  `unit` varchar(45) NOT NULL,
+  PRIMARY KEY (`unit`),
+  UNIQUE KEY `unit_UNIQUE` (`unit`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `units`
+--
+
+LOCK TABLES `units` WRITE;
+/*!40000 ALTER TABLE `units` DISABLE KEYS */;
+/*!40000 ALTER TABLE `units` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `navn` varchar(45) NOT NULL,
+  `post_nr` int NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `adress` varchar(45) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_user_role_idx` (`role`),
+  CONSTRAINT `fk_user_role` FOREIGN KEY (`role`) REFERENCES `role` (`role`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-08 10:45:11
