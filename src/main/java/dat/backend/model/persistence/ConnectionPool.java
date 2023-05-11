a93a523b75bace965d05f8b41c51a8f68f3a2398
TheRealJackiBoi <jslam@oulund.dk>
2023-05-12 00:11:32 +0200

    update

--- src/main/java/dat/backend/model/persistence/ConnectionPool.java
+++ src/main/java/dat/backend/model/persistence/ConnectionPool.java
@@ -12,7 +12,7 @@ public class ConnectionPool
 {
     // TODO: Change access credentials for MySql server as needed below:
 
-    private HikariDataSource ds;
+  private HikariDataSource ds;
     private static String USER = "root";
     private static String PASSWORD = "root";
     private static String URL = "jdbc:mysql://localhost:3306/fog_carport";
