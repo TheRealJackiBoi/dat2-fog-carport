60cf16be4a154360ab864d806246ad35040f36da
asphixia <william2450>
2023-05-12 12:25:37 +0200

    added role mapper and entity

---
+++ src/main/java/dat/backend/model/entities/Role.java
@@ -0,0 +1,26 @@
+package dat.backend.model.entities;
+
+public class Role {
+
+    private String role;
+
+    public Role(String role){
+        this.role = role;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "role='" + role + '\'' +
+                '}';
+    }
+}
