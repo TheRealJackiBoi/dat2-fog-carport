7c7c6b25a514f9d9ca19f8853f359f4cf2dcc945
TheRealJackiBoi <jslam@oulund.dk>
2023-05-16 16:06:52 +0200

    CreateOrder doGet working + added favicon to pagetemplate😍

--- src/main/java/dat/backend/control/CreateOrder.java
+++ src/main/java/dat/backend/control/CreateOrder.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ordering", urlPatterns = {"/ordering"} )
+@WebServlet(name = "ordering", urlPatterns = {"/bestil"} )
 public class CreateOrder extends HttpServlet
 {
     private ConnectionPool connectionPool;
@@ -35,8 +35,21 @@ public class CreateOrder extends HttpServlet
         if (user == null){
             request.getRequestDispatcher("index.jsp").forward(request, response);
         }else {
-            int orderId;
+
+            int currentOrderId;
             int userId = user.getId();
+            if (session.getAttribute("submit").equals("update")) {
+
+            }
+            else if (session.getAttribute("submit").equals("order")) {
+
+            }
+            else {
+                // You shouldn't end up here
+                request.setAttribute("errormessage", "Hmmm, der gik noget galt🤔");
+                request.getRequestDispatcher("error.jsp").forward(request, response);
+            }
+
             double length = Double.parseDouble(request.getParameter("length"));
             double width = Double.parseDouble(request.getParameter("width"));
             double height = Double.parseDouble(request.getParameter("height"));
@@ -69,22 +82,30 @@ public class CreateOrder extends HttpServlet
         if (user == null){
             request.getRequestDispatcher("index.jsp").forward(request, response);
         }else {
-            int orderId = (int) session.getAttribute("orderId");
-            int userId = ((User) session.getAttribute("user")).getId();
-            double length = Double.parseDouble(request.getParameter("length"));
-            double width = Double.parseDouble(request.getParameter("width"));
-            double height = Double.parseDouble(request.getParameter("height"));
-            double s_width = 0;
-            double s_length = 0;
 
-            try {
-                Order order = OrdersFacade.getOrderByOrderId(orderId, connectionPool);
-                session.setAttribute("orderId", orderId);
-                request.getRequestDispatcher("index.jsp").forward(request, response);
+            // checking if there's an order in the making
+            // if true -> get values from orders and forward to jsp form
+            // else forward without values
+            if (session.getAttribute("current_order_id") != null) {
+                int currentOrderId = (int) session.getAttribute("current_order_id");
+                int userId = ((User) session.getAttribute("user")).getId();
 
-            } catch (DatabaseException e) {
-                request.setAttribute("errormessage", e.getMessage());
-                request.getRequestDispatcher("error.jsp").forward(request, response);
+                try {
+                    Order order = OrdersFacade.getOrderByOrderId(currentOrderId, connectionPool);
+
+                    //forwarding variables to jsp form
+                    request.setAttribute("length", order.getCarportLength());
+                    request.setAttribute("width", order.getCarportWidth());
+                    request.setAttribute("height", order.getCarportHeight());
+
+                    request.getRequestDispatcher("WEB-INF/ordering.jsp").forward(request, response);
+                } catch (DatabaseException e) {
+                    request.setAttribute("errormessage", e.getMessage());
+                    request.getRequestDispatcher("error.jsp").forward(request, response);
+                }
+            }
+            else {
+                request.getRequestDispatcher("WEB-INF/ordering.jsp").forward(request, response);
             }
 
         }
--- src/main/webapp/WEB-INF/ordering.jsp
+++ src/main/webapp/WEB-INF/ordering.jsp
@@ -12,34 +12,35 @@
 
     <jsp:body>
 
-        <h1>Design din carport</h1>
-        <form class="container">
-            <form action="ordering" name="ordering" method="post">
+        <h1 class="text-center">Design din carport</h1>
+
+        <div class="container">
+            <form name="ordering" method="post">
                 <div class="form inline">
-                <div class="col-5 col-md-5 col-lg-2 align-self-end ">
+                <div class="mx-auto col-10 col-md-8 col-lg-3">
                     <label for="enterHeight">Højde i meter</label>
                     <input type="number" step="0.01" class="form-control" id="enterHeight" name="height" placeholder="Enter Height">
                 </div><br>
-                <div class="col-5 col-md-5 col-lg-2 align-self-center">
+                <div class="mx-auto col-10 col-md-8 col-lg-3">
                     <label for="enterWidth">Bredde i meter</label>
                     <input type="number" step="0.01" class="form-control" id="enterWidth" name="width" placeholder="Enter Width">
                 </div><br>
                 </div>
-                <div class="col-5 col-md-5 col-lg-2 align-self-end">
+                <div class="mx-auto col-10 col-md-8 col-lg-3">
                     <label for="enterLength">Længde i meter</label>
                     <input type="number" step="0.01" class="form-control" id="enterLength" name="length" placeholder="Enter Length">
                 </div><br>
-                <div class="col-5 col-md-5 col-lg-2 align-self-end">
-                    <textarea for="textArea" rows="3"  placeholder="Skrive her"></textarea>
+                <div class="mx-auto col-10 col-md-8 col-lg-3">
+                    <textarea for="textArea" rows="3"  placeholder="Skrive her" class="w-100"></textarea>
                 </div><br>
-                <div class="d-grid gap-2 d-md-block">
-                    <button class="btn btn-danger" onclick="location.href='index.jsp'">Cancel</button>
-                    <button type="button" class="btn btn-primary">Opdater</button>
-                    <button type="submit" formaction="ordering" formmethod="post" class="btn btn-success" value="order">Bestil</button>
+                <div class="mx-auto mx-auto col-10 col-md-8 col-lg-3 d-flex justify-content-between">
+                    <button type="submit" formaction="cancelorder" formmethod="post" class="btn btn-danger">Cancel</button>
+                    <button type="submit" formaction="ordering" formmethod="post" name="submit" value="update" class="btn btn-primary">Opdater</button>
+                    <button type="submit" formaction="ordering" formmethod="post" name="submit" value="order" class="btn btn-success">Bestil</button>
                 </div>
             </form>
+        </div>
 
-        </form>
 
 
 
