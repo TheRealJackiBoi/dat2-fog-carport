f0e17938281c1914000b8a64e1fa11916362cbab
artiecodes <bjarke.dehlie@gmail.com>
2023-05-17 14:14:51 +0200

    curly brackets fix

--- src/main/java/dat/backend/model/persistence/UserMapper.java
+++ src/main/java/dat/backend/model/persistence/UserMapper.java
@@ -3,56 +3,45 @@ package dat.backend.model.persistence;
 import dat.backend.model.entities.User;
 import dat.backend.model.exceptions.DatabaseException;
 
-import javax.xml.crypto.Data;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class UserMapper
-{
-    static User login(String email, String password, ConnectionPool connectionPool) throws DatabaseException
-    {
+public class UserMapper {
+    static User login(String email, String password, ConnectionPool connectionPool) throws DatabaseException {
         Logger.getLogger("web").log(Level.INFO, "");
 
         User user = null;
 
         String sql = "SELECT * FROM user WHERE email = ? AND password = ?";
 
-        try (Connection connection = connectionPool.getConnection())
-        {
-            try (PreparedStatement ps = connection.prepareStatement(sql))
-            {
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
                 ps.setString(1, email);
                 ps.setString(2, password);
                 ResultSet rs = ps.executeQuery();
-                if (rs.next())
-                {
+                if (rs.next()) {
                     String role = rs.getString("role");
                     int id = rs.getInt("id");
                     user = new User(id, email, password, role);
-                } else
-                {
+                } else {
                     throw new DatabaseException("Wrong username or password");
                 }
             }
-        } catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             throw new DatabaseException(ex, "Error logging in. Something went wrong with the database");
         }
         return user;
     }
 
-    static void createUser(String email, String password, String name, int zip, String city, String address, String role, ConnectionPool connectionPool) throws DatabaseException
-    {
+    static void createUser(String email, String password, String name, int zip, String city, String address, String role, ConnectionPool connectionPool) throws DatabaseException {
         Logger.getLogger("web").log(Level.INFO, "");
         User user;
         String sql = "INSERT INTO user (email, password, name, zip, city, adress, role) values (?,?,?,?,?,?,?)";
-        try (Connection connection = connectionPool.getConnection())
-        {
-            try (PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS))
-            {
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
                 ps.setString(1, email);
                 ps.setString(2, password);
                 ps.setString(3, name);
@@ -64,8 +53,7 @@ public class UserMapper
                 ps.executeUpdate();
             }
         }
-        catch (SQLException ex)
-        {
+        catch (SQLException ex) {
             throw new DatabaseException(ex, "Could not insert username into database");
         }
     }
