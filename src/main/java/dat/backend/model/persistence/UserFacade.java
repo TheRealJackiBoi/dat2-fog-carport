a5780dd025adb2fb150eb578a6f6a83baec3cae1
TheRealJackiBoi <jslam@oulund.dk>
2023-05-18 15:23:58 +0200

    customer_see_orders working, missing orderconfirm route

--- src/main/java/dat/backend/model/persistence/UserFacade.java
+++ src/main/java/dat/backend/model/persistence/UserFacade.java
@@ -3,6 +3,8 @@ package dat.backend.model.persistence;
 import dat.backend.model.entities.User;
 import dat.backend.model.exceptions.DatabaseException;
 
+import java.util.List;
+
 public class UserFacade {
     public static User login(String email, String password, ConnectionPool connectionPool) throws DatabaseException {
         return UserMapper.login(email, password, connectionPool);
