d0df368576b8924bd850839b6862dd9fd4a5c044
Jack <33868006+TheRealJackiBoi@users.noreply.github.com>
2023-05-18 12:43:36 +0200

    Update LinkItemListMaterialsVariantsMapper.java

--- src/main/java/dat/backend/model/persistence/LinkItemListMaterialsVariantsMapper.java
+++ src/main/java/dat/backend/model/persistence/LinkItemListMaterialsVariantsMapper.java
@@ -16,7 +16,7 @@ public class LinkItemListMaterialsVariantsMapper {
 
     static void addLink(int itemListId, int materialVariantsId, ConnectionPool connectionPool) throws  DatabaseException{
 
-        String sql = "INSERT INTO item_list (item_list_id, material_variants_id) VALUES (?,?)";
+        String sql = "INSERT INTO link_item_list_material_variants (item_list_id, material_variants_id) VALUES (?,?)";
 
         try(Connection connection = connectionPool.getConnection()){
             try(PreparedStatement ps = connection.prepareStatement(sql)){
